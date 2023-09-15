2a0916df40eca630cc32fabeda70bf00823f11aa
Sonhongmin <sonhongmin0907@gmail.com>
2023-09-15 15:18:21 +0900

    feat: Print fizzbuzz
    Print fizzbuzz if i is multiple of 15.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,7 +9,9 @@ def do_fizzbuzz():
     """
     
     for i in range(1,20):
-        if i % 3 == 0:
+        if i % 15 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
             print('fizz')
         elif i % 5 == 0:
             print('buzz')
